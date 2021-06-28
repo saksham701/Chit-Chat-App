aac5c5159f114c02d2b5bc4e7f3705b02c481062
saksham701 <iit2019022@iiita.ac.in>
2021-06-28 16:25:46 +0530

    fixed bug in port

--- app.js
+++ app.js
@@ -62,5 +62,5 @@ io.on('connection', (socket) => {
     
 });
 
-const port = 3000 || process.env.PORT;
+const port =  (process.env.PORT || 3000);
 server.listen(3000, () => console.log('server running on port' + port));
